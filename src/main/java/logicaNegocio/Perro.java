6c6173e679dba197e0dff6e9e2678226ce7442ed
santiag015 <santiago.university21.9@gmail.com>
2024-11-18 08:22:14 -0500

    Clase Perro

--- src/main/java/logicaNegocio/Perro.java
+++ src/main/java/logicaNegocio/Perro.java
@@ -1,5 +1,56 @@
 package logicaNegocio;
 
-public class Perro ez{
+public class Perro extends Animal {
+
     // TODO: Aquí va tu código
+    private String emitirSonido;
+    private String obtenerDieta;
+    private String TipoHabitat;
+    
+    public Perro(String emitirSonido, String obtenerDieta, String TipoHabitat) {
+        this.emitirSonido = emitirSonido;
+        this.obtenerDieta = obtenerDieta;
+        this.TipoHabitat = TipoHabitat;
+    }
+
+    public String getEmitirSonido() {
+        return emitirSonido;
+    }
+
+    public String getObtenerDieta() {
+        return obtenerDieta;
+    }
+
+    public String getTipoHabitat() {
+        return TipoHabitat;
+    }
+
+    public void setEmitirSonido(String emitirSonido) {
+        this.emitirSonido = emitirSonido;
+    }
+
+    public void setObtenerDieta(String obtenerDieta) {
+        this.obtenerDieta = obtenerDieta;
+    }
+
+    public void setTipoHabitat(String TipoHabitat) {
+        this.TipoHabitat = TipoHabitat;
+    }
+    
+       public String emitirSonido() {
+        this.emitirSonido = "Ladrido";
+        return this.emitirSonido;
+    }
+
+    public String obtenerDieta() {
+        this.obtenerDieta = "Omnivoro";
+        return this.obtenerDieta;
+    }
+
+    public String TipoHabitat() {
+        this.TipoHabitat = "Terrestre";
+        return this.TipoHabitat;
+    } 
+    
 }
+
